1185fcb3669c29acefa6cc79e5493c466c3d509f
Ji Eun <jieun1416@gmail.com>
2021-05-03 23:41:42 +0900

    commit unit-test-1week

---
+++ unit-test-1week/src/test/java/ChampionTest.java
@@ -0,0 +1,108 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ChampionTest {
+    private List<Champion> championList = new ArrayList<Champion>();
+
+    @Before
+    public void setUp() {
+
+        //5개의 챔피언 객체를 만듭니다.
+        Champion topChamp = new Champion("다리우스", "탑");
+        Champion jungleChamp = new Champion("리신", "정글");
+        Champion midChamp = new Champion("르블랑", "미드");
+        Champion adcChamp = new Champion("베인", "바텀");
+        Champion supportChamp = new Champion("레오나", "바텀");
+
+        //앞서 만든 List 에 각 챔피언을 추가합니다.
+        championList.add(topChamp);
+        championList.add(jungleChamp);
+        championList.add(midChamp);
+        championList.add(adcChamp);
+        championList.add(supportChamp);
+    }
+
+    //List<String>을 생성하고 값이 비어 있는지를 테스트 empty()
+    @Test
+    public void givenCollectionWhenEmptyCorrect() {
+        List<String> emptyList = new ArrayList<>();
+    }
+
+    //notNullValue 활용한 테스트
+    @Test
+    public void notNullCheck() {
+        String lck = "LCK";
+    }
+
+    //nullValue 활용한 테스트
+    @Test
+    public void givenStringWhenNullIsCorrect() {
+        String lck = null;
+    }
+
+    //문자열 관련 테스트 anyOf, containsString, endWith
+    @Test
+    public void testForRelatedString() {
+        String sampleString1 = "Player Focus";
+        String sampleString2 = "Player point";
+        String startString = "Player";
+        String endString = "point";
+    }
+
+    //부동소수점 범위 closeTo 테스트
+    @Test
+    public void testForFloatingPoint() {
+
+    }
+
+    //anything 테스트
+    @Test
+    public void shouldNotErrorGetReference() {
+
+    }
+
+    //객체 크기 검증 테스트 hasSize
+    @Test
+    public void shouldChampionCountFive() {
+
+    }
+
+    //서폿 챔피언은 타릭이어야 한다라는 조건으로 테스트 코드 작성
+    @Test
+    public void shouldSupportChampionIsTaric() {
+        Champion supportChamp = new Champion("타릭", "바텀");
+
+    }
+
+    //hasProperty 활용하여 속성이 포함되어 있는지 테스트
+    @Test
+    public void shouldHasPropertyPosition() {
+
+    }
+
+    //hasToString 활용 테스트
+    @Test
+    public void shouldHaveSomeChampName() {
+        List<String> champListNames = Arrays.asList("루시안", "애쉬", "렉사이", "갈리오", "모르가느", "블라디미르");
+
+    }
+
+    //property와 value가 같은지 테스트
+    @Test
+    public void shouldHaveSamePropertyAndValue() {
+        List<String> championNames1 = Arrays.asList("루시안", "애쉬", "렉사이", "갈리오", "모르가나", "블라디미르");
+        List<String> championNames2 = Arrays.asList("루시안", "애쉬", "렉사이", "갈리오", "모르가나", "블라디미르");
+
+    }
+
+    //탑 챔피언은 다리우스여야 한다라는 조건으로 테스트 코드 작성, stream 활용예
+    @Test
+    public void shouldTopChampionIsDarius() {
+
+    }
+
+}
